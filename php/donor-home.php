443ac6cf5d1c8c43e65f88da3776429bb3d57236
WaiS <waisiong144@gmail.com>
2021-10-02 21:39:23 +0800

    First Draft of Donor Homepage
    included CSS, JS, PHP as well as a IMG folder

--- php/donor-home.php
+++ php/donor-home.php
@@ -1,15 +1,296 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <title>Donor Homepage</title>
-</head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, maximum-scale=1">
+<title>Donor Homepage</title>
+<link rel="icon" href="favicon.png" type="image/png">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="donation-home.css" rel="stylesheet" type="text/css"> 
 
-<body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</head>
+<body style="background-color:#00ffaa;"">
+  <div class="container">
+    <div class="inner_section">
+ <div class="row">
+      <div class="col-md-6">        
+        <h2 style="padding-top: 250px;">Anonym-us Angel</h2>
+        <h3>A fundraising platform which allows you to donate to an anonymous project under a selected category so that every project is supported.</h3> 							 
+      </div>
+            <div class="col-md-6"> <img class="img-responsive" src="img/angel.png" align=""> </div>
+
+    </div>
+	     </div>
+  </div> 
+  </div>
+<!--Aboutus--> 
+
+<section id="Portfolio" class="content"> 
+  
+  <!-- Container -->
+  <div class="container portfolio_title"> 
+    
+    <!-- Title -->
+    <div class="section-title">
+      <h2>Categories</h2>    
+    </div>
+    <!--/Title --> 
+    
+  </div>
+  <!-- Container -->
+  
+  <div class="portfolio-top"></div>
+  
+  <!-- Portfolio Filters -->
+  <div class="portfolio"> 
+    
+    <div id="filters" class="sixteen columns">
+      <ul class="clearfix">
+        <li><a id="all" href="#" data-filter="*" class="active">
+          <h5>All</h5>
+          </a></li>
+        <li><a class="" href="#" data-filter=".food">
+          <h5>Food Supply</h5>
+          </a></li>
+        <li><a class="" href="#" data-filter=".education">
+          <h5>Education</h5>
+          </a></li>
+        <li><a class="" href="#" data-filter=".covid">
+          <h5>Covid-19</h5>
+          </a></li>
+        <li><a class="" href="#" data-filter=".disaster">
+          <h5>Natural Disaster</h5>
+          </a></li> 
+      </ul>
+    </div>
+    <!--/Portfolio Filters --> 
+    
+    <!-- Portfolio Wrapper -->
+    <div class="isotope fadeInLeft animated wow grid" style="position: relative; overflow: hidden; height: 480px;" id="portfolio_wrapper">  
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four   disaster isotope-item effect-oscar">
+     
+        <div class="portfolio_img"> 
+    <img src="img/natural.png"  alt="Natural Disaster"> </div> 
+      <figcaption>    
+        <div>
+          <h2><span>Disaster</span></h2>
+              <p>Save the Earth and our People</p>
+          </a>
+        </div>
+      </figcaption>
+        </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item-->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(337px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  food isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/food.png" alt="food supply"> </div>
+          <figcaption>    
+        <div>
+          <h2>Food <span>Supply</span></h2>
+              <p>Dont Let them Starve</p>
+          </a>
+        </div>
+      </figcaption>
+        </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(674px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  education  isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/education2.png" alt="education"> </div>
+       <figcaption>   
+        <div>
+          <h2><span>Education</span></h2>
+              <p>Kids Deserve to go to School</p>
+          </a>
+        </div>
+      </figcaption>
+        </figure>
+      <!--/Portfolio Item--> 
+      
+      <!-- Portfolio Item-->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(1011px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  education  nature web isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/education.png" alt="education"> </div>
+         <figcaption>   
+        <div>
+          <h2><span>Education</span></h2>
+              <p>Kids Deserve to go to School</p>
+          </a>
+        </div>
+      </figcaption>
+      </figure>
+      <!-- Portfolio Item --> 
+      
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 240px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  covid isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/covid2.png" alt="covid19"> </div>
+       <figcaption>   
+        <div>
+          <h2>Covid <span>19</span></h2>
+              <p>Buy them Mask and Supplies</p>
+          </a>
+        </div>
+      </figcaption>
+      </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(337px, 240px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four covid web isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/covid1.png" alt="covid19"> </div>
+       <figcaption>   
+        <div>
+          <h2>Covid <span>19</span></h2>
+              <p>Buy them Mask and Supplies</p>
+          </a>
+        </div>
+      </figcaption>
+      </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item  -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(674px, 240px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  disaster web isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/natural3.png" alt="Natural Disaster"> </div>       
+       <figcaption>   
+        <div>
+          <h2> <span>Disaster</span></h2>
+              <p>Save the Earth and our People</p>
+          </a>
+        </div>
+      </figcaption>
+       </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(1011px, 240px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four   disaster isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/natural2.png" alt="Natural Disaster"> </div>       
+      <figcaption>    
+        <div>
+          <h2> <span>Disaster</span></h2>
+              <p>Save the Earth and our People</p>
+          </a>
+        </div>
+      </figcaption>
+        </figure>
+      <!--/Portfolio Item --> 
+      
+    </div>
+    <!--/Portfolio Wrapper --> 
+    
+  </div>
+  <!--/Portfolio Filters -->
+  
+  <div class="portfolio_btm"></div>
+  
+  
+  <div id="project_container">
+    <div class="clear"></div>
+    <div id="project_data"></div>
+  </div>
+ 
+  
+</section>
+<!--/Portfolio --> 
+  
+
+<!--Service-->
+<section  id="service">
+  <div class="container">
+    <h2>How To Be An Anonym-Us Angel?</h2>
+    <div class="service_wrapper">
+      <div class="row">
+        <div class="col-md-3">
+		<div class="delay-03s animated wow  zoomIn"><img src="img/1.png" alt=" 1" style="padding-left: 60px;padding-bottom: 10px;"> </div>
+          <div class="service_block">
+            <br>
+            <h3 class="animated fadeInUp wow">Choose a category </h3>
+            <p class="animated fadeInDown wow">Which has the project that you would like to support</p>
+          </div>
+        </div>
+        <div class="col-md-3"> 
+		<div class="delay-03s animated wow zoomIn"><img src="img/2.png" alt="2" style="padding-left: 60px; padding-bottom: 10px;"> </div> 
+		<div class="service_block">
+            <h3 class="animated fadeInUp wow">Donate</h3>
+            <p class="animated fadeInDown wow">and make payment  </p>
+          </div>
+        </div>
+        <div class="col-md-3">
+		<div class="delay-03s animated wow zoomIn"><img src="img/3.png" alt="3" style="padding-left: 60px;"></div>
+          <div class="service_block">
+            <br>
+            <h3 class="animated fadeInUp wow">Get Appreciated</h3>
+            <p class="animated fadeInDown wow">by receving a Thank You note truly by us and a receipt</p>
+          </div>
+        </div>
+		  <div class="col-md-3">
+		<div class="delay-03s animated wow zoomIn"><img src="img/4.png" alt="4" style="padding-left: 60px;"></div>
+          <div class="service_block">
+            <br><br>
+            <h3 class="animated fadeInUp wow">Understand</h3>
+            <p class="animated fadeInDown wow">more about the project that you chose to donate</p>
+          </div>
+        </div>
+      </div> 
+	 
+    </div>
+  </div>
+</section>
+<!--Service--> 
+
+
+
+ 
+</section>
+
+<section id="qna">
+  <div class="accordion-body">
+  <div class="accordion">
+    <h1>Frequently Asked Questions</h1>
+    <hr>
+    <div class="ques">
+      <div class="label">Who am I donating to?</div>
+      <div class="content">You'll be donating to a random NGO/Companies dedicated to the categories that you have chosen previously. All the donations done will be anonymous as information of the donors will not be exposed to anyone.</div>
+    </div>
+    <hr>
+    <div class="ques">
+      <div class="label">Who is the team behind Anonym-Us Angels</div>
+      <div class="content">The team behind Anonym-Us Angels consists of 7 members which are Marcus (Leader), Chin Wai Siong, Darren, Wen Jun, Jia Xin, Amber and also Javier.
+      </div>
+    </div>
+    <hr>
+    <div class="ques">
+      <div class="label">What is the company’s motto?</div>
+      <div class="content">The company's motto is "Support the Poor and Share Privilleges Equally" </div>
+    </div>
+    <hr>
+    <div class="ques">
+      <div class="label">What are the categories of the donations?</div>
+      <div class="content">There are FOUR donation categories in total which are Covid-19, Natural Disaster, Education, and lastly, Food Supply. </div>
+    </div>
+    <hr>
+    <div class="ques">
+      <div class="label">Why is the donation being done anonymously?</div>
+      <div class="content">The donation is being done anonymously is to protect our client's identity(donors) so that the donors can donate freely and without any disturbance or external issues.</div>
+    </div>
+    <hr>
+    <div class="ques">
+      <div class="label">Who designed this wonderful homepage?</div>
+      <div class="content">Totally not Wai Siong</div>
+    </div>
+    <hr>
+  </div>
+  </div>
+</section>
+
+<script type="text/javascript" src="donor-home.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script> 
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.js"></script>
+<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script> 
+<script type="text/javascript" src="js/wow.js"></script> 
+<script type="text/javascript" src="js/custom.js"></script>
+
 </body>
 </html>
