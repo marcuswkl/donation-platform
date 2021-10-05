fad7a699f42ba4b5b893d1a939bd9f661dfad47c
Amber1004 <ambersyy@gmail.com>
2021-10-05 20:13:55 +0800

    Add javascript to retrieve thankyou.php after 8 seconds
    The javascript is added into loading.php

--- loading.php
+++ loading.php
@@ -17,5 +17,10 @@
     </div>
     <img src = "img/loading.gif">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+       setTimeout(function(){
+            window.location.href = 'thankyou.php';
+         }, 8000);
+     </script>
   </body>
 </html>
