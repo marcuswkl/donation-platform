9488fa40fe86c17457aefcce066ef0a03d6c4006
Amber1004 <ambersyy@gmail.com>
2021-10-05 20:28:49 +0800

    Added "Learn more" button

--- thankyou.php
+++ thankyou.php
@@ -24,7 +24,7 @@
         </p>
     </div>
 
-    
+    <button type="button" class="btn btn-primary btn-lg">Learn more</button>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
