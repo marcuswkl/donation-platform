846902298b8fd482c0f7ee39b4e2653695435ea5
Amber1004 <ambersyy@gmail.com>
2021-10-05 20:18:40 +0800

    Added title

--- thankyou.php
+++ thankyou.php
@@ -11,7 +11,9 @@
     <title>Thank you</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="text-center">
+        <h1 class="display-2">Thank you for your donation</h1>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
