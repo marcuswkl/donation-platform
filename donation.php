45e7276090c81c02e4adf659be49a6fe5e0dfe8e
Amber1004 <ambersyy@gmail.com>
2021-10-05 19:35:21 +0800

    Add "Enter amount: " form
    Need to adjust the text field area

--- donation.php
+++ donation.php
@@ -16,6 +16,19 @@
     <div class="text-center">
       <h1 class="display-2">You Are Supporting EDUCATION</h1>
     </div>
+    <!-- Donation Amount Form -->
+    <form>  
+      <div class="row">
+        <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Enter amount: </label>
+          <div class="col-sm-10">
+            <div class="input-group input-group-lg " >
+              <span class="input-group-text" id="inputGroup-sizing-lg">RM</span>
+              <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="0.00">
+            </div>
+          </div>
+      </div>
+    </form>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
