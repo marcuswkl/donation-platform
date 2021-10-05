dabcd0d49d05f4b4936b3520c8a89cc9119ed8aa
Amber1004 <ambersyy@gmail.com>
2021-10-05 20:52:00 +0800

    Change the loading spinner

--- loading.php
+++ loading.php
@@ -15,7 +15,11 @@
     <div class="text-center">
         <h1 class="display-1">Loading</h1>
     </div>
-    <img src = "img/loading.gif">
+    <div class="text-center">
+      <div class="spinner-border m-5" role="status">
+        <span class="sr-only"></span>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
        setTimeout(function(){
