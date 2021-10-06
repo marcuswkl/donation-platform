ed7fe6163a1a9fee7d3f977f05c50add5f44ae00
kojiaxin <kojiaxin@hotmail.com>
2021-10-06 14:33:45 +0800

    Add showcase with css

--- project-info/project1.php
+++ project-info/project1.php
@@ -27,6 +27,59 @@
     <title>Project Info</title>
   </head>
 
-  <body>
+<body>
+
+
+  <!-- Showcase -->
+  <section class="p-5">
+    <div class="row text-left g-4">
+      <div class="col-lg-7">
+        <div id="carouselImage" class="carousel slide carousel-space" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselImage" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselImage" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselImage" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="img/showcase1.png" class="img-fluid d-block w-100" alt="Children Picture 1">
+            </div>
+            <div class="carousel-item">
+              <img src="img/showcase2.png" class="img-fluid d-block w-100" alt="Children Picture 2">
+            </div>
+            <div class="carousel-item">
+              <img src="img/showcase3.png" class="img-fluid d-block w-100" alt="Children Picture 3">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselImage" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselImage" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div class="col-lg-5 main-info">
+          <h1 class="d-flex location"><img src="img/location-icon.png" class="img-fluid d-block mx-3 location-icon" alt="Location Icon"><strong>Kajang, Selangor</strong></h1>
+          <h1 class="lead showcase-title">Empower Children Through Education</h1>
+          <p class="fundraiser">Fundraiser >> Bunway</p>
+          <div class="d-flex">
+            <img src="img/goal-icon.png" class="img-fluid d-block mx-3 goal-icon" alt="Goal Icon">
+            <div class="d-flex flex-column">
+              <div>
+                <p class="align-items-left funds"><strong>RM3,000 raised</strong> of RM 5,000</p>
+              </div>
+              <div class="d-flex">
+                <div class="goal-progress"></div>
+                <div class="goal-bar"></div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+
 </body>
 </html>
