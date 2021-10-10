e283a326a51eda8962551ed12df5a6af6bbc5044
InstaKarma4 <lawwenjun@gmail.com>
2021-10-10 15:34:27 +0800

    First draft of category pages

--- php/categories/food-supply.php
+++ php/categories/food-supply.php
@@ -1,2 +1,209 @@
-food-supply
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="../../css/food-supply.css" rel="stylesheet">
+    <title>Food Supply</title>
+</head>
+
+<body>
+    <div class="d-flex top-banner align-items-center">
+        <div class="banner-text">
+            <h1>FOOD SUPPLY</h1>
+            <p>Send a warm meal and aid food security in poverty-stricken areas.</p>
+        </div>
+        <div class="float-right align-self-end">
+            <img class="banner-img" src="../../resources/img/food-supply-banner.png" alt="Food Supply illustration">
+        </div>
+    </div>
+
+    <div class="d-flex toolbar justify-content-end align-items-center">
+        <div class="search">
+            <form class="form-inline">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search..." aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-success" type="button">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="toolbar-child">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                States - All
+            </a>
+
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item active" href="#">All</a></li>
+                <li><a class="dropdown-item" href="#">Johor</a></li>
+                <li><a class="dropdown-item" href="#">Kedah</a></li>
+                <li><a class="dropdown-item" href="#">Kelantan</a></li>
+                <li><a class="dropdown-item" href="#">Melaka</a></li>
+                <li><a class="dropdown-item" href="#">Negeri Sembilan</a></li>
+                <li><a class="dropdown-item" href="#">Pahang</a></li>
+                <li><a class="dropdown-item" href="#">Penang</a></li>
+                <li><a class="dropdown-item" href="#">Perak</a></li>
+                <li><a class="dropdown-item" href="#">Perlis</a></li>
+                <li><a class="dropdown-item" href="#">Sabah</a></li>
+                <li><a class="dropdown-item" href="#">Sarawak</a></li>
+                <li><a class="dropdown-item" href="#">Klang Valley (Selangor &amp; Kuala Lumpur)</a></li>
+                <li><a class="dropdown-item" href="#">Terengganu</a></li>
+            </ul>
+        </div>
+
+        <button type="button" class="btn btn-primary toolbar-child">DONATE</button>
+    </div>
+
+    <div class="container-md project-list">
+        <div class="row justify-content-center">
+            <div class="col-md px-5 py-2">
+                <a href="#">
+                    <div class="d-flex align-items-center pt-3">
+                        <img src="https://via.placeholder.com/1920x1080" class="project-list-image">
+                    </div>
+                    <div class="d-inline-flex align-items-center">
+                        <img src="../../resources/img/location_icon.png" class="project-list-icon">
+                        <div class="project-list-location">Kajang, Selangor</div>
+                    </div>
+                    <div class="project-list-title h3">Empower Children Through Education</div>
+                    <div class="project-list-fundraiser">Fundraiser: Bunway</div>
+                    <div class="project-list-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a distinctio pariatur itaque fuga necessitatibus soluta, commodi cum laborum voluptatibus, in, aperiam voluptate? Eveniet dolores in ea amet, cupiditate at!</div>
+                    <div class="d-inline-flex align-items-center">
+                        <div class="project-list-funds-raised">RM 6900.69 raised</div>
+                        <div class="project-list-funds-raised-total">&nbsp;of RM 100,000.00</div>
+                    </div>
+                    <div class="progress project-list-progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="69" aria-valuemin="0" aria-valuemax="100" style="width: 69%"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md px-5 py-2">
+                <a href="#">
+                    <div class="d-flex align-items-center pt-3">
+                        <img src="https://via.placeholder.com/1920x1080" class="project-list-image">
+                    </div>
+                    <div class="d-inline-flex align-items-center">
+                        <img src="../../resources/img/location_icon.png" class="project-list-icon">
+                        <div class="project-list-location">Kajang, Selangor</div>
+                    </div>
+                    <div class="project-list-title h3">Empower Children Through Education</div>
+                    <div class="project-list-fundraiser">Fundraiser: Bunway</div>
+                    <div class="project-list-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a distinctio pariatur itaque fuga necessitatibus soluta, commodi cum laborum voluptatibus, in, aperiam voluptate? Eveniet dolores in ea amet, cupiditate at!</div>
+                    <div class="d-inline-flex align-items-center">
+                        <div class="project-list-funds-raised">RM 6900.69 raised</div>
+                        <div class="project-list-funds-raised-total">&nbsp;of RM 100,000.00</div>
+                    </div>
+                    <div class="progress project-list-progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="69" aria-valuemin="0" aria-valuemax="100" style="width: 69%"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md px-5 py-2">
+                <a href="#">
+                    <div class="d-flex align-items-center pt-3">
+                        <img src="https://via.placeholder.com/1920x1080" class="project-list-image">
+                    </div>
+                    <div class="d-inline-flex align-items-center">
+                        <img src="../../resources/img/location_icon.png" class="project-list-icon">
+                        <div class="project-list-location">Kajang, Selangor</div>
+                    </div>
+                    <div class="project-list-title h3">Empower Children Through Education</div>
+                    <div class="project-list-fundraiser">Fundraiser: Bunway</div>
+                    <div class="project-list-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a distinctio pariatur itaque fuga necessitatibus soluta, commodi cum laborum voluptatibus, in, aperiam voluptate? Eveniet dolores in ea amet, cupiditate at!</div>
+                    <div class="d-inline-flex align-items-center">
+                        <div class="project-list-funds-raised">RM 6900.69 raised</div>
+                        <div class="project-list-funds-raised-total">&nbsp;of RM 100,000.00</div>
+                    </div>
+                    <div class="progress project-list-progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="69" aria-valuemin="0" aria-valuemax="100" style="width: 69%"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md px-5 py-2">
+                <a href="#">
+                    <div class="d-flex align-items-center pt-3">
+                        <img src="https://via.placeholder.com/1920x1080" class="project-list-image">
+                    </div>
+                    <div class="d-inline-flex align-items-center">
+                        <img src="../../resources/img/location_icon.png" class="project-list-icon">
+                        <div class="project-list-location">Kajang, Selangor</div>
+                    </div>
+                    <div class="project-list-title h3">Empower Children Through Education</div>
+                    <div class="project-list-fundraiser">Fundraiser: Bunway</div>
+                    <div class="project-list-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a distinctio pariatur itaque fuga necessitatibus soluta, commodi cum laborum voluptatibus, in, aperiam voluptate? Eveniet dolores in ea amet, cupiditate at!</div>
+                    <div class="d-inline-flex align-items-center">
+                        <div class="project-list-funds-raised">RM 6900.69 raised</div>
+                        <div class="project-list-funds-raised-total">&nbsp;of RM 100,000.00</div>
+                    </div>
+                    <div class="progress project-list-progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="69" aria-valuemin="0" aria-valuemax="100" style="width: 69%"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md">
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-outline-success load-more-btn" type="button">Load more...</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="d-flex info-tab text-center justify-content-evenly">
+        <div class="info">
+            <p id="info-weeks" class="info-number h1">2</p>
+            <p class="info-label h4">Weeks</p>
+        </div>
+
+        <div class="info">
+            <p id="info-funds-raised" class="info-number h1">RM696,969.69</p>
+            <p class="info-label h4">Funds Raised</p>
+        </div>
+
+        <div class="info">
+            <p id="info-countries" class="info-number h1">1</p>
+            <p class="info-label h4">Country</p>
+        </div>
+
+        <div class="info">
+            <p id="info-organizations" class="info-number h1">18</p>
+            <p class="info-label h4">Organizations</p>
+        </div>
+    </div>
+
+    <div class="container-fluid bottom-banner">
+        <div class="row">
+            <div class="col-md justify-content-center">
+                <div class="d-inline-flex align-items-center">
+                    <img src="../../resources/img/motto_icon.png" alt="Motto icon" class="bottom-banner-motto-icon">
+                    <div class="bottom-banner-title">OUR MOTTO</div>
+                </div>
+                <div class="bottom-banner-motto">Every organization should be noticed as everyone is playing their part in helping the society.</div>
+
+            </div>
+
+            <div class="col-md">
+                abc
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
+<!-- Illustration by <a href="https://icons8.com/illustrations/author/60896eeb088aa7001087bb4d">Rosina Gavrilash</a> from <a href="https://icons8.com/illustrations">Ouch!</a> -->
