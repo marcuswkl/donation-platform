f66196265c9f64ef47a730cdecc421bd0a9da556
Amber1004 <ambersyy@gmail.com>
2021-10-14 14:14:26 +0800

    Added donation list table

--- donor-donation.php
+++ donor-donation.php
@@ -17,6 +17,38 @@
           <h1 class="display-2 text-center">Donations</h1>
     </div>
 
+    <table class="table table-bordered border-white table-hover">
+        <thead class="tableheader text-center">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Fundraiser</th>
+            <th scope="col">Project</th>
+            <th scope="col">Amount (RM)</th>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+          </tr>
+        </thead>
+
+        <tbody class="tabledata text-center">
+            <tr>
+              <th scope="row" class="text-center">1</th>
+              <td>DE Foundation</td>
+              <td>Children's Home</td>
+              <td>500.00</td>
+              <td>8/5/2021</td>
+              <td>10:32 AM</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>EduFundX</td>
+              <td>Student Scholarship</td>
+              <td>20.00</td>
+              <td>15/7/2021</td>
+              <td>12:30 PM</td>
+            </tr>
+        </tbody>
+    </table>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
