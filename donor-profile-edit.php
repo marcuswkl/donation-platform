2a1dc037cbbe16cbcf1810748b8dee575525074b
Amber1004 <ambersyy@gmail.com>
2021-10-14 14:07:14 +0800

    Added title and font

--- donor-profile-edit.php
+++ donor-profile-edit.php
@@ -9,10 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="css/donor-profile-edit.css">
+    <link href='https://fonts.googleapis.com/css?family=Titillium Web' rel='stylesheet'>
     <title>Edit Donor Profile</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <div class="text-center p-5">
+        <h1 class="display-2">Edit Profile</h1>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
