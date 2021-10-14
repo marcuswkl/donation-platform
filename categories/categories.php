7dbdc31c909bfef6a74c25c1c9f627fcbb3ef34d
InstaKarma4 <lawwenjun@gmail.com>
2021-10-14 17:52:49 +0800

    Added nested header and footer
    Also added header and footer for all category (Jun) pages

--- categories/categories.php
+++ categories/categories.php
@@ -12,50 +12,54 @@
 </head>
 
 <body>
-    <!-- Categories -->
-    <div class="h1 text-center">Categories</div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md">
-                <a href="covid-19.php" class="text-decoration-none" style="overflow:hidden">
-                    <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-1">
-                        <div class="overlay">
-                            <div class="text">Covid-19</div>
+    <?php include '../head-nested.php'; ?>
+    <div class="py-2">
+        <!-- Categories -->
+        <div class="h1 text-center">Categories</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <a href="covid-19.php" class="text-decoration-none" style="overflow:hidden">
+                        <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-1">
+                            <div class="overlay">
+                                <div class="text">Covid-19</div>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md">
-                <a href="education.php" class="text-decoration-none" style="overflow:hidden">
-                    <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-2">
-                        <div class="overlay">
-                            <div class="text">Education</div>
+                    </a>
+                </div>
+                <div class="col-md">
+                    <a href="education.php" class="text-decoration-none" style="overflow:hidden">
+                        <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-2">
+                            <div class="overlay">
+                                <div class="text">Education</div>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md">
-                <a href="food-supply.php" class="text-decoration-none" style="overflow:hidden">
-                    <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-3">
-                        <div class="overlay">
-                            <div class="text">Food Supply</div>
+                    </a>
+                </div>
+                <div class="col-md">
+                    <a href="food-supply.php" class="text-decoration-none" style="overflow:hidden">
+                        <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-3">
+                            <div class="overlay">
+                                <div class="text">Food Supply</div>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md">
-                <a href="natural-disaster.php" class="text-decoration-none" style="overflow:hidden">
-                    <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-4">
-                        <div class="overlay">
-                            <div class="text">Natural Disaster</div>
+                    </a>
+                </div>
+                <div class="col-md">
+                    <a href="natural-disaster.php" class="text-decoration-none" style="overflow:hidden">
+                        <div class="d-flex align-items-center justify-content-center text-light full-height bg-img-4">
+                            <div class="overlay">
+                                <div class="text">Natural Disaster</div>
+                            </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <?php include '../foot-nested.php'; ?>
 </body>
 
 </html>
--- categories/education.php
+++ categories/education.php
@@ -11,6 +11,7 @@
 </head>
 
 <body>
+    <?php include '../head-nested.php'; ?>
     <div class="d-flex top-banner align-items-center">
         <div class="banner-text">
             <h1>EDUCATION</h1>
@@ -254,6 +255,8 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <?php include '../foot-nested.php'; ?>
+
 </body>
 
 </html>
