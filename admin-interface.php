b8a4d82acb6b39363be47cdab4e1c539ffb8375f
Dino1716 <91538900+Dino1716@users.noreply.github.com>
2021-10-14 15:10:32 +0800

    Update admin-interface.php

--- admin-interface.php
+++ admin-interface.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link href = "../CSS/admin-interface.css" type = "text/css" rel = "stylesheet">
+    <link href = "../css/admin-interface.css" type = "text/css" rel = "stylesheet">
     <title>Admin Interface</title>
 </head>
 
@@ -61,7 +61,7 @@
     <!--One Fundraiser Description -->
     <div class="container-md mt-3 d-flex">
         
-        <img src="../icons/placeholder.png" alt="image" class = "fundraiser ">
+        <img src="../resources/img/placeholder.png" alt="image" class = "fundraiser ">
         <div class = "my-auto">
             <ul>
                 <ol>Fundraiser Name: Derek Edmonds Foundation</ol>
@@ -81,7 +81,7 @@
 
     <div class="container-md mt-3 d-flex">
         
-        <img src="../icons/placeholder.png" alt="image" class = "fundraiser ">
+        <img src="../resources/img/placeholder.png" alt="image" class = "fundraiser ">
         <div class = "my-auto">
             <ul>
                 <ol>Fundraiser Name: Derek Edmonds Foundation</ol>
@@ -99,7 +99,7 @@
 
     <div class="container-md mt-3 d-flex">
         
-        <img src="../icons/placeholder.png" alt="image" class = "fundraiser ">
+        <img src="../resources/img/placeholder.png" alt="image" class = "fundraiser ">
         <div class = "my-auto">
             <ul>
                 <ol>Fundraiser Name: Derek Edmonds Foundation</ol>
