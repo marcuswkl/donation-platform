043d8b9100b8ae2c0853ea4f247c0cf78c90d1e6
kojiaxin <kojiaxin@hotmail.com>
2021-10-14 17:31:37 +0800

    Modify Header and Footer

--- foot.php
+++ foot.php
@@ -1,42 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Bootstrap Icons CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+<link href="../css/foot.css" type="text/css" rel="stylesheet">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <!-- Bootstrap Icons CDN -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
-    <link href="../css/foot.css" type="text/css" rel="stylesheet">
-
-</head>
-
-<body>
-    <footer class="p-3 footer" style="background-color: #B2E1CC; border-top: 2px solid black">
-        <div class="d-flex justify-content-start align-items-center">
-            <div>
-                <p class="lead text-center">Copyright &copy; 2021 Anonym-Us Angels. All Rights Reserved.</p>
-            </div>
-
-            <div class="text-center ms-auto">
-                <p class="text-center ms-auto fw-bold">Anonym-Us Angels</p>
-                <div class="d-inline-flex">
-                    <a href="#"><i class="bi bi-facebook" style="margin-right: 0.5em;"></i></a>
-                    <a href="#"><i class="bi bi-instagram" style="margin-right: 0.5em;"></i></a>
-                    <a href="#"><i class="bi bi-twitter" style="margin-right: 0.5em;"></i></a>
-                </div>
-
-            </div>
+<footer class="p-3 footer" style="background-color: #B2E1CC; border-top: 2px solid black">
+    <div class="d-flex justify-content-start align-items-center">
+        <div>
+            <p class="lead text-center">Copyright &copy; 2021 Anonym-Us Angels. All Rights Reserved.</p>
         </div>
 
+        <div class="text-center ms-auto">
+            <p class="text-center ms-auto fw-bold">Anonym-Us Angels</p>
+            <div class="d-inline-flex">
+                <a href="#"><i class="bi bi-facebook" style="margin-right: 0.5em;"></i></a>
+                <a href="#"><i class="bi bi-instagram" style="margin-right: 0.5em;"></i></a>
+                <a href="#"><i class="bi bi-twitter" style="margin-right: 0.5em;"></i></a>
+            </div>
 
-
-
-
-    </footer>
-
-</body>
-
-</html>
+        </div>
+    </div>
+</footer>
--- head.php
+++ head.php
@@ -1,50 +1,35 @@
-<html>
+<link href="../css/head.css" type="text/css" rel="stylesheet">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<nav class="navbar navbar-expand-md navbar-light head">
+    <div class="container topnav-container">
+        <a href="#"> <img src="../resources/img/Logo.png" alt="logo" class="mx-2 logo"></a>
 
-    <link href="../css/head.css" type="text/css" rel="stylesheet">
-</head>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<body>
-    <nav class="navbar navbar-expand-md navbar-light head">
-        <div class="container">
-            <a href="#"> <img src="../resources/img/Logo.png" alt="logo" class="mx-2 logo"></a>
+        <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="d-flex navbar-nav me-auto">
+                <li class="nav-item menu">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <!-- <li class="nav-item menu">
+                    <a href="#" class="nav-link">[Placeholder]</a>
+                </li>
 
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="d-flex navbar-nav me-auto">
-                    <li class="nav-item menu">
-                        <a href="#" class="nav-link">Home</a>
-                    </li>
-
-                    <!-- <li class="nav-item menu">
-                        <a href="#" class="nav-link">[Placeholder]</a>
-                    </li>
-
-                    <li class="nav-item menu">
-                        <a href="#" class="nav-link">[Placeholder]</a>
-                    </li> -->
-                </ul>
-            </div>
-
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="d-flex navbar-nav ms-auto">
-                    <li class="nav-item menu ">
-                        <a href="#" class="nav=link"><img src="../resources/img/Logout.png" alt="logout" class="logout">Log Out</a>
-                    </li>
-                </ul>
-            </div>
+                <li class="nav-item menu">
+                    <a href="#" class="nav-link">[Placeholder]</a>
+                </li> -->
+            </ul>
         </div>
-    </nav>
-</body>
 
-
-</html>
+        <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="d-flex navbar-nav ms-auto">
+                <li class="nav-item menu ">
+                    <a href="#" class="nav=link"><img src="../resources/img/Logout.png" alt="logout" class="logout">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
