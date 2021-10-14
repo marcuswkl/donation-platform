cf970781d3dab28c6633b3c6e41f1ec4d8193184
Amber1004 <ambersyy@gmail.com>
2021-10-14 13:39:00 +0800

    Adjust spacing, size and change font

--- donation.php
+++ donation.php
@@ -9,18 +9,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="css/donation.css">
+    <link href='https://fonts.googleapis.com/css?family=Titillium Web' rel='stylesheet'>
     <title>Donation</title>
   </head>
   <body>
     <!-- Title -->
-    <div class="text-center">
+    <div class="text-center m-5">
       <h1 class="display-2">You Are Supporting EDUCATION</h1>
     </div>
     <!-- Donation Amount Form -->
     <form>  
-      <div class="row">
-        <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Enter amount: </label>
-          <div class="col-sm-10">
+      <div class="row m-2 justify-content-md-center">
+        <label for="colFormLabelLg" class="col-sm-auto col-form-label col-form-label-lg">Enter amount: </label>
+          <div class="col-sm-auto">
             <div class="input-group input-group-lg " >
               <span class="input-group-text" id="inputGroup-sizing-lg">RM</span>
               <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="0.00">
@@ -50,10 +51,11 @@
           </label>
         </div>
       </div>
-      <div class="form-group row">
-        <div class="col-sm-10">
-          <button type="submit" class="btn btn-primary">Donate</button>
-        </div>
+
+      <div class="text-center"> 
+        <button type="submit" class="btn btn-primary m-3">
+          <a href = "payment.php" class="text-dark btn">Donate</a>
+        </button>
       </div>
     </form>
 
