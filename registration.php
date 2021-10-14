9231c4797a114add1f0c9f49580ba58bc7fc973a
Dino1716 <91538900+Dino1716@users.noreply.github.com>
2021-10-14 18:01:34 +0800

    Change Pathing to CSS and resources

--- registration.php
+++ registration.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "../CSS/registration.css">
-    <title>Login</title>
+    <link rel = "stylesheet" href = "./CSS/registration.css">
+    <title>Registraion</title>
 </head>
 
 <body>
@@ -64,7 +64,8 @@
         </div>
 
         
-    <br>
+    <br><br><br>
+    
     <?php include "foot.php"?>
 
     
