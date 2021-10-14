8f4bda5ad1f38900f7ea7f46720beb6bac0cb6fe
Amber1004 <ambersyy@gmail.com>
2021-10-14 14:13:44 +0800

    Added title

--- donor-donation.php
+++ donor-donation.php
@@ -13,7 +13,9 @@
     <title>Donor Donation</title>
   </head>
   <body>
-    
+    <div class="justify-content-md-center p-5">
+          <h1 class="display-2 text-center">Donations</h1>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
