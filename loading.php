8d804ea8df772ced89850450d0e2408b86f69418
Amber1004 <ambersyy@gmail.com>
2021-10-14 13:57:02 +0800

    Add font, adjust spacing

--- loading.php
+++ loading.php
@@ -9,10 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="css/loading.css">
+    <link href='https://fonts.googleapis.com/css?family=Titillium Web' rel='stylesheet'>
     <title>Loading</title>
   </head>
   <body>
-    <div class="text-center">
+    <div class="text-center p-5">
         <h1 class="display-1">Loading</h1>
     </div>
     <div class="text-center">
@@ -21,10 +22,6 @@
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script>
-       setTimeout(function(){
-            window.location.href = 'thankyou.php';
-         }, 8000);
-     </script>
+    <script src="js/loading.js"></script>
   </body>
 </html>
