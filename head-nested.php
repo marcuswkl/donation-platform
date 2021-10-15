f1ef82b76184150804b69ce2ac1eb4e0519b1eac
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-10-15 19:14:36 +0800

    Add link to donor home page for nested header

--- head-nested.php
+++ head-nested.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-md navbar-light head">
     <div class="container topnav-container">
-        <a href="#"> <img src="../resources/img/Logo.png" alt="logo" class="mx-2 logo"></a>
+        <a href="../donor-home.php"> <img src="../resources/img/Logo.png" alt="logo" class="mx-2 logo"></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
             <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navmenu">
             <ul class="d-flex navbar-nav me-auto">
                 <li class="nav-item menu">
-                    <a href="#" class="nav-link">Home</a>
+                    <a href="../donor-home.php" class="nav-link">Home</a>
                 </li>
 
                 <!-- <li class="nav-item menu">
