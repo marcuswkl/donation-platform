b5bed1710c045452f7e1fcaff8e992fbc9525b3a
WaiS <waisiong144@gmail.com>
2021-10-31 20:19:40 +0800

    added DBController and edited boilerplate

---
+++ DbController.php
@@ -0,0 +1,17 @@
+<?php 
+  $host = 'localhost';
+  $data = 'anonym_us';
+  $user = 'forall';         
+  $pass =  'forall123';      
+  $chrs = 'utf8mb4';
+  $attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+  $opts =
+  [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+
+  
+
+?>
