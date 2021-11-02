33558dc2ddd2e89d16957b18911cae71c3a2d813
WaiS <waisiong144@gmail.com>
2021-11-02 23:08:26 +0800

    if user = fundraiser then direct to fundraiser regis php

---
+++ js/registration.js
@@ -0,0 +1,10 @@
+function display() { 
+    var rd1 = document.getElementById("fundraiser");
+
+    if(rd1.checked==true) {
+        location.href = 'fundraiser-registration.php';
+    }
+    else{
+        
+    }
+}
