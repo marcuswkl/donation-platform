0cd73e5c10b76527089110dc0aded0d8ec18cc20
Amber1004 <ambersyy@gmail.com>
2021-11-20 19:41:07 +0800

    Fetch and display donation amount from database

--- donation-confirmation.php
+++ donation-confirmation.php
@@ -5,4 +5,8 @@ require_once 'DbController.php';
 $input_array = array("Empower Children Through Education", "Mask up against Covid", "Mask up against Covid 2.0", "Mask up against Covid 3.0"); 
 $rand_keys = array_rand($input_array , 2); 
 
+foreach($result as $row){
+    $amount = $row['amount'];
+}
+
 ?>
