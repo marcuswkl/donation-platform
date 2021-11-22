81dc79eca60ad4cf9cb232ad1fdba6811ec24a54
kojiaxin <kojiaxin@hotmail.com>
2021-11-23 06:44:04 +0800

    Display user name and email according to session

--- form-handlers/login_backend.php
+++ form-handlers/login_backend.php
@@ -16,23 +16,28 @@ if(isset($_POST['email']) && isset($_POST['pwd'])){
     $un  = $row['name'];
     $pw  = $row['password'];
     $ty  = $row['type'];
+    $em  = $row['email'];
 
     if (password_verify( $pw_temp, $pw))
     {
       session_start();
 
-      $_SESSION['name'] = $fn;
+      $_SESSION['name'] = $un;
       $_SESSION['type'] = $ty;
+      $_SESSION['email'] = $em;
 
-      if($_SESSION['type'] == 'donor'){
-        echo "<script type='text/javascript'>alert('Login Successfully');</script>";
-        echo "<script type='text/javascript'>window.location.href = './donor-home.php';</script>";
-      }else if($_SESSION['type'] == 'fundraiser'){
-        echo "<script type='text/javascript'>window.location.href = './fundraiser-home.php';</script>";
-      }else if($_SESSION['type'] == 'admin'){
-        echo "<script type='text/javascript'>window.location.href = './admin-interface.php';</script>";
+        if(isset($_SESSION['name'])){
+          if(isset($_SESSION['email'])){
+            if($_SESSION['type'] == 'donor'){
+              echo "<script type='text/javascript'>alert('Login Successfully');</script>";
+              echo "<script type='text/javascript'>window.location.href = './donor-home.php';</script>";
+            }else if($_SESSION['type'] == 'fundraiser'){
+              echo "<script type='text/javascript'>window.location.href = './fundraiser-home.php';</script>";
+            }else if($_SESSION['type'] == 'admin'){
+              echo "<script type='text/javascript'>window.location.href = './admin-interface.php';</script>";
+            }
+        }
       }
-
     }
     else echo "<script type='text/javascript'>alert('Incorrect username/ password');</script>";
   }
