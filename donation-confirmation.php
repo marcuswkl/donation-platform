78182e6b51aa92e6336781c074529ea99601c3a1
Amber1004 <ambersyy@gmail.com>
2021-11-25 23:28:17 +0800

    Try to save the project into donation_history
    To show the donor donation table

--- donation-confirmation.php
+++ donation-confirmation.php
@@ -1,9 +1,40 @@
 <?php
 
 require_once 'DbController.php';
+require_once 'form-handlers/login_backend.php';
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+$servername = "localhost";
+    $username = "forall";
+    $password = "forall123";
+    $dbname = "anonym_us";
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
 $input_array = array("Mask up against Covid", "Mask up against Covid 2.0", "Mask up against Covid 3.0"); 
-$rand_keys = array_rand($input_array , 2); 
+$rand_keys = array_rand($input_array , 2);
+$project = $input_array[$rand_keys[0]];
+$id = $_SESSION['id'];
+
+// $sql = "UPDATE donation_history
+//         SET fundraiserId, projectId = (
+//             SELECT projectId, fundraiserId 
+//             FROM project 
+//             WHERE projectName = '$project')
+//         WHERE fundraiserId, projectId IS NULL";
+echo $input_array[$rand_keys[0]];
+
+$sql = "WITH id AS (
+            SELECT projectid,fundraiserid FROM project WHERE projectName = '$project' )
+UPDATE donation_history
+SET fundraiserId = fundraiserid
+AND projectId = projectid
+WHERE userId = '$id'
+AND fundraiserId, projectId IS NULL
+";       
+    
+$result1 = $conn->query($sql);    
 
 $query = "SELECT * FROM donation_history";
 $query = $pdo->query($query);
