21a1615a6e5fe44ca62634e39131fc3449939b51
WaiS <waisiong144@gmail.com>
2021-11-25 14:41:29 +0800

    userId is doneeeee

--- form-handlers/donation_backend.php
+++ form-handlers/donation_backend.php
@@ -1,25 +1,25 @@
 <?php 
 
 require_once './DbController.php';
+require_once 'login_backend.php';
+session_start();
+
 
 if(isset($_POST['amt'])){
     $amount = sanitise($pdo,$_POST['amt']);
     $date = date("Y-m-d H:i:s");
     $date2 = sanitise($pdo,$date);
     $tbl_name = 'donation_history';
+    $id = $_SESSION['id'];
 
     $query = "INSERT INTO $tbl_name (donationId, date, amount, projectId, userId, fundraiserId) 
-    VALUES(NULL, $date2, $amount, NULL, NULL, NULL)";
+    VALUES(NULL, $date2, $amount, NULL, $id, NULL)";
+
+    echo $query;
     $result = $pdo->query($query);
 
     echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
     
 }
-
-function sanitise($pdo, $str)
-{
-  $str = htmlentities($str);
-  return $pdo->quote($str);
-}	
 
 ?>
