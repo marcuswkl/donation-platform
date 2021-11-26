9639ad765a2c9964b4e5ec97f7751b098cb10f15
Amber1004 <ambersyy@gmail.com>
2021-11-26 08:34:16 +0800

    Add sql to update the projectId, fundraiserId in donation_history table
    Not working

--- donation-confirmation.php
+++ donation-confirmation.php
@@ -6,6 +6,7 @@ require_once 'form-handlers/login_backend.php';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
 $servername = "localhost";
     $username = "forall";
     $password = "forall123";
@@ -17,22 +18,16 @@ $rand_keys = array_rand($input_array , 2);
 $project = $input_array[$rand_keys[0]];
 $id = $_SESSION['id'];
 
-// $sql = "UPDATE donation_history
-//         SET fundraiserId, projectId = (
-//             SELECT projectId, fundraiserId 
-//             FROM project 
-//             WHERE projectName = '$project')
-//         WHERE fundraiserId, projectId IS NULL";
-echo $input_array[$rand_keys[0]];
-
 $sql = "WITH id AS (
-            SELECT projectid,fundraiserid FROM project WHERE projectName = '$project' )
+            SELECT projectId,fundraiserId FROM project WHERE projectName = '$project' )
 UPDATE donation_history
-SET fundraiserId = fundraiserid
-AND projectId = projectid
-WHERE userId = '$id'
-AND fundraiserId, projectId IS NULL
-";       
+SET donation_history.fundraiserId = project.fundraiserId
+AND donation_history.projectId = project.projectId
+WHERE donation_history.userId = '$id'
+AND donation_history.fundraiserId, donation_history.projectId IS NULL
+";    
+
+echo $sql;
     
 $result1 = $conn->query($sql);    
 
